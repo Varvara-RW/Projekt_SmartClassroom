7e13f4a97af6b30e1cf22a20592083ee588b7f61
Varvara-RW <gusevav@gmx.de>
2026-06-07 11:08:18 +0200

    first step to generate and fil the database with values

---
+++ pythonTestDatabase/ModuleMain.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+
+# print(time.asctime(clock))
+
+def addTuple(datensatz):
+	# get clock somehow
+	
+	# data = [b,p,t, clock]
+	sql = ''' INSERT INTO measurements(brightness, pressure, temperature) VALUES(?,?,?)  '''
+
+	cur = con.cursor()
+	cur.execute(sql, datensatz)
+
+	con.commit()
+	con.close()
+
+con = sqlite3.connect("measurements.db")
+
+
+cur = con.cursor()
+try:
+	cur.execute("CREATE TABLE measurements(brightness TEXT, pressure REAL, temperature REAL, timestamp TEXT DEFAULT CURRENT_TIMESTAMP)")
+except Exception:
+        pass
+
+
+res = cur.execute("SELECT name FROM sqlite_master")
+res.fetchone()
+
+res1 = cur.execute("SELECT temperature FROM measurements")
+res1.fetchall()
+
+datensatz = ('hell', 932.9, 22.1)
+datensatzID = addTuple(datensatz)
+
+# def readTuple
+# def deleteTuple
+# def highest.temperature{}
+# highest and lowest values as well as the average of each attribute
